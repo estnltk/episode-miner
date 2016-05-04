68a163d5ee3dee0d0fb4917b0d882d96b8e6bfd9
paultammo <paul.tammo@gmail.com>
2016-05-04 10:15:29 +0300

    improved setup and docs

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='episode-miner',
       description = "Finds frequent episodes in event sequence.",
       version='1.0.0',
-      install_requires = ['estnltk', 'pyahocorasick'],
+      install_requires = ['pyahocorasick'],
       author = 'Paul Tammo',
       author_email='paul.tammo@gmail.com',
       packages = find_packages(),
