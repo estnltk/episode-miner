3ee2abd477f1e9abf1764283d8c866db7a9d3a9e
paultammo <paul.tammo@gmail.com>
2016-05-13 20:04:56 +0300

    few more EventSequence tests

--- episode_miner/tests/test_event_sequence.py
+++ episode_miner/tests/test_event_sequence.py
@@ -2,6 +2,7 @@ import unittest
 from episode_miner import Event, EventSequence, EventText, EventTagger
 
 class EventTest(unittest.TestCase):
+    
     def test_initialization(self):
         event = Event('midagi', 23)
         self.assertEqual(event.event_type, 'midagi')
@@ -49,24 +50,34 @@ class EventSequenceTest(unittest.TestCase):
                             {'term': 'seitse'}]    
         event_tagger = EventTagger(event_vocabulary, search_method='naive', conflict_resolving_strategy='ALL')
         event_text = EventText('Arv kakskümmend viis on suurem kui seitse.', event_tagger=event_tagger)
+
+        event_sequence = EventSequence(event_text=event_text, classificator='term', time_scale='start')
+        self.assertEqual(event_sequence.end, 42)
+        self.assertEqual(event_sequence.start, 0)
+        self.assertListEqual(event_sequence.sequence_of_events, 
+                             [Event('kakskümmend viis', 4), Event('seitse', 35)])
+
+        event_sequence = EventSequence(event_text=event_text, classificator='term', time_scale='start', 
+                                       start=3, end=35)
+        self.assertEqual(event_sequence.end, 35)
+        self.assertEqual(event_sequence.start, 3)
+        self.assertListEqual(event_sequence.sequence_of_events, 
+                             [Event('kakskümmend viis', 4)])
+
         event_sequence = EventSequence(event_text=event_text, classificator='term', time_scale='cstart')
         self.assertEqual(event_sequence.end, 22)
         self.assertEqual(event_sequence.start, 0)
-        self.assertEqual(event_sequence.sequence_of_events[0].event_type, 'kakskümmend viis')
-        self.assertEqual(event_sequence.sequence_of_events[0].event_time, 4)
-        self.assertEqual(event_sequence.sequence_of_events[1].event_type, 'seitse')
-        self.assertEqual(event_sequence.sequence_of_events[1].event_time, 20)
+        self.assertListEqual(event_sequence.sequence_of_events, 
+                             [Event('kakskümmend viis', 4), Event('seitse', 20)])
 
         event_sequence = EventSequence(event_text=event_text, classificator='term', time_scale='wstart')
-        self.assertEqual(event_sequence.end, 7)
         self.assertEqual(event_sequence.start, 0)
-        self.assertEqual(event_sequence.sequence_of_events[0].event_type, 'kakskümmend viis')
-        self.assertEqual(event_sequence.sequence_of_events[0].event_time, 1)
-        self.assertEqual(event_sequence.sequence_of_events[1].event_type, 'seitse')
-        self.assertEqual(event_sequence.sequence_of_events[1].event_time, 5)
-
+        self.assertEqual(event_sequence.end, 7)
+        self.assertListEqual(event_sequence.sequence_of_events, 
+                             [Event('kakskümmend viis', 1), Event('seitse', 5)])
 
         event_text = EventText('Sündmusteta tekst.', event_tagger=event_tagger)
+
         event_sequence = EventSequence(event_text=event_text, classificator='term', time_scale='cstart')
         self.assertEqual(event_sequence.start, 0)
         self.assertEqual(event_sequence.end, 18)
@@ -76,4 +87,3 @@ class EventSequenceTest(unittest.TestCase):
         self.assertEqual(event_sequence.start, 0)
         self.assertEqual(event_sequence.end, 3)
         self.assertEqual(len(event_sequence.sequence_of_events), 0)
-
