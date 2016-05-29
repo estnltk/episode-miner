08822bf749b2ac9241ed066df7d4a32cd369dfee
paultammo <paul.tammo@gmail.com>
2016-05-29 23:43:01 +0300

    abs_Support, rel_support

--- episode_miner/__init__.py
+++ episode_miner/__init__.py
@@ -1,4 +1,4 @@
 from .event_tagger import EventTagger, TERM, CSTART, WSTART
 from .event_text import EventText
 from .event_sequence import Event, EventSequence
-from .winepi import Episode, collection_of_frequent_episodes
+from .winepi import Episode, find_sequential_episodes, abs_support, rel_support
