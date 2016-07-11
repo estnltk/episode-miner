72bf6a58005db39b29f973c7cd28e797eb3eec19
paultammo <paul.tammo@gmail.com>
2016-07-11 10:11:10 +0300

    improved import

--- episode_miner/__init__.py
+++ episode_miner/__init__.py
@@ -1,3 +1,3 @@
-from .event_tagger import EventTagger, TERM, CSTART, WSTART
+from .event_tagger import EventTagger, KeywordTagger, RegexTagger, TERM, CSTART, WSTART
 from .event_text import EventText
 from .event_sequences import Episode, Episodes, Event, EventSequence, EventSequences
